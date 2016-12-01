cbf968dcd3286e3b4a24acb0eb0c3efd7c36145a
thucnhi2411 <let@lafayette.edu>
2016-12-01 01:05:13 -0500

    Avoid putting two plants in one ccell

--- PlantVsZombie.java
+++ PlantVsZombie.java
@@ -15,7 +15,7 @@ public class PlantVsZombie extends GraphicsProgram
     public static int 
     APPLICATION_WIDTH = 1000,
     APPLICATION_HEIGHT = 720;   
-    
+
     // instance variables
     Grass[][] grass = new Grass[5][8]; // array of grass
     private int sun = 0; // the number of sun
@@ -24,116 +24,89 @@ public class PlantVsZombie extends GraphicsProgram
     private GImage peashooterChoice; // the choice of peashooter
     private GPoint lastPoint;
     private boolean 
-        sunflowerDragged = false,
-        peashooterDragged = false;
-    
+    sunflowerDragged = false,
+    peashooterDragged = false;
+
     public void init(){
         drawGraphics();
-        
+
     }
+
     /** start dragging if the object is pressed on */
     public void mousePressed(GPoint point){
         if (sunflowerChoice.contains(point)) sunflowerDragged = true;
         if (peashooterChoice.contains(point)) peashooterDragged = true;
         lastPoint = point;
     }
-    
+
     /** move the object when it is dragged */
     public void mouseDragged(GPoint point){
         if (sunflowerDragged){
             sunflowerChoice.move(point.getX()-lastPoint.getX(),
-            point.getY()-lastPoint.getY());
+                point.getY()-lastPoint.getY());
         }
-        
+
         if (peashooterDragged){
             peashooterChoice.move(point.getX()-lastPoint.getX(),
-            point.getY()-lastPoint.getY());
+                point.getY()-lastPoint.getY());
         }
-        
-        
+
         lastPoint = point;
     }
-    
-     /** stop dragging */
+
+    /** stop dragging */
     public void mouseReleased(GPoint point) {
-        double x = point.getX();
+        double x = point.getX(); // the location of the mouse
         double y = point.getY();
-        double xLocation = 0;
-        double yLocation = 0;
+        double xLocation = 0; // the x location of the plant
+        double yLocation = 0; // the y location of the plant
+        int col = 0; // the column of the grass cell
+        int row = 0; // the row of the grass cell
+        
+        // find the location to place to plant 
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 8; j++) { 
+                if (x>j*100+160 && x<(j+1)*100+160 && y>i*100+200 && y<(i+1)*100+200) {
+                    xLocation = j*100+10+200;
+                    yLocation = i*100+50+200;
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+        
         // if the sunflower is dragged
         if (sunflowerDragged){
-            if (x<160 || x>960 || y<200 || y>700) {
+            if (grass[row][col].isFilled() == true || x<160 || x>960 || y<200 || y>700) {
                 sunflowerChoice.setLocation(170,50);
                 sunflowerDragged = false;
             } else {
                 Sunflower sunflower = new Sunflower(this);
-                add(sunflower,xLocation(x),yLocation(y));
+                add(sunflower,xLocation,yLocation);
                 new Thread(sunflower).start();
                 sunflowerChoice.setLocation(170,50);
-                fillGrass(xLocation(x),yLocation(y));
+                grass[row][col].fill();
                 sunflowerDragged = false;
             }
         }
-        
+
         // if the peashooter is dragged
         if (peashooterDragged){
-            if (x<160 || x>960 || y<200 || y>700) {
+            if (grass[row][col].isFilled() == true || x<160 || x>960 || y<200 || y>700) {
                 peashooterChoice.setLocation(270,50);
                 peashooterDragged = false;
             } else {
                 Peashooter peashooter = new Peashooter(this);
-                add(peashooter,xLocation(x),yLocation(y));
+                add(peashooter,xLocation,yLocation);
                 new Thread(peashooter).start();
                 peashooterChoice.setLocation(270,50);
-                fillGrass(xLocation(x),yLocation(y));
+                grass[row][col].fill();
                 peashooterDragged = false;
             }
         }
-        
+
     }
-    
-    // find the x location in the grid
-    public int xGrass(double x){
-        int output=0;
-        for (int i = 0; i<8;i++){
-            if (x>i*100+160 && x<(i+1)*100+160)  output = i;
-        }
-        return output;
-    }
-    // find the y location in the grid
-    public int yGrass(double y){
-        int output=0;
-        for (int i = 0; i<5;i++){
-            if (y>i*100+200 && y<(i+1)*100+200)  output = i;
-        }
-        return output;
-    }
-    
-    // fill the grass if the plant is placed on the grid of grass
-    public void fillGrass(double x, double y){
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 8; j++) { 
-                if (x>j*100+160 && x<(j+1)*100+160 && y>i*100+200 && y<(i+1)*100+200) grass[i][j].fill();
-            }
-        }
-    }
-    
-    // find the x location in the grid
-    private double xLocation(double x){
-        double xLocation = 0;
-        for (int i = 0; i<8;i++){
-            if (x>i*100+160 && x<(i+1)*100+160)  xLocation = i*100+10+200;
-        }
-        return xLocation;
-    }
-    // find the y location in the grid
-    private double yLocation(double y){
-        double yLocation = 0;
-        for (int i = 0; i<5;i++){
-            if (y>i*100+200 && y<(i+1)*100+200)  yLocation = i*100+50+200;
-        }
-        return yLocation;
-    }
+
     // increase the sun
     public void increaseSun(){
         sun++;
@@ -143,27 +116,26 @@ public class PlantVsZombie extends GraphicsProgram
     private void drawGraphics(){
         // draw the grass
         drawGrass();
-        
+
         // draw the house
         GRect house = new GRect(40,500);
         house.setFilled(true);
         house.setFillColor(new Color(239, 102, 45));
         add(house,20,200);
-        
+
         GImage houseLabel = new GImage("houseLabel.png");
         houseLabel.setSize(40,500);
         add(houseLabel,15,200);
-        
-        
+
         // draw the choice of plant
         drawPlantChoice();
-        
+
         // draw the board showing the sun acquired
         GRect sunLabel = new GRect(120,80);
         sunLabel.setFilled(true);
         sunLabel.setFillColor(new Color(255, 166, 77));
         add(sunLabel,20,40);
-        
+
         sunNumber = new GLabel("0");
         sunNumber.setFont(new Font("Sanserif", Font.BOLD, 30));
         sunNumber.setColor(Color.BLACK);
@@ -172,22 +144,21 @@ public class PlantVsZombie extends GraphicsProgram
         GImage sun = new GImage("sun.png");
         sun.setSize(40,40);
         add(sun,60,80);
-        
+
         sunflowerChoice = new GImage("sunflower.gif");
         sunflowerChoice.setSize(80,80);
         add(sunflowerChoice,170,50);
-        
+
         // draw the peashooter choice
         peashooterChoice = new GImage("peashooter.gif");
         peashooterChoice.setSize(80,80);
         add(peashooterChoice,270,50);
-        
+
         GImage zombie = new GImage("zombie.gif");
         zombie.setSize(80,80);
         add(zombie,370,50);
     }
-    
-    
+
     // draw the choice of plants
     private void drawPlantChoice(){
         GRect[] choice = new GRect[6];
@@ -198,7 +169,7 @@ public class PlantVsZombie extends GraphicsProgram
             add(choice[i],160 + i*100,40);
         }
     }
-    
+
     // draw the grass
     private void drawGrass() {
         for (int i = 0; i < 5; i++) {
@@ -216,7 +187,7 @@ public class PlantVsZombie extends GraphicsProgram
                     grass[i][j].changeColor();   
                 }
             }
-           
+
         }
     }
 }
